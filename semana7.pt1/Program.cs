3bd4d428248d60b50c0739890d34d36fcac84166
David19 <davidleonel911@gmail.com>
2025-01-24 17:52:53 -0500

    tareas sem7

---
+++ semana7.pt1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // esta es la expresion que vamos a evaluar
+        string expresion = "{7+(8*5)-[(9-7)+(4+1)]}";
+
+        // Nos ayudamos de la función VerificarBalance 
+        bool balanceada = VerificarBalance(expresion);
+
+      
+        if (balanceada)
+        {
+            Console.WriteLine("La expresion si esta balanceada :)");
+        }
+        else
+        {
+            Console.WriteLine("La expresion no esta balanceada :(");
+        }
+    }
+
+
+    
+    static bool VerificarBalance(string expresion)
+    {
+        // lleva el seguimiento de los caracteres de apertura
+        Stack<char> pila = new Stack<char>();
+
+
+        foreach (char caracter in expresion)
+        {
+            // agregamos un caracter de apertura a la pila
+            if (caracter == '(' || caracter == '[' || caracter == '{')
+            {
+                pila.Push(caracter);
+            }
+            // Si encontramos un carácter de cierre, hacemos las validaciones
+            else if (caracter == ')' || caracter == ']' || caracter == '}')
+            {
+                //si hay un cierre sin su apertura la pila estara vacia 
+                if (pila.Count == 0)
+                {
+                    return false;
+                }
+
+              
+                char tope = pila.Pop();
+
+                // Verificamos si el carácter de apertura coincide con el cierre
+                if (!Coinciden(tope, caracter))
+                {
+                    return false;
+                }
+            }
+        }
+
+
+        return pila.Count == 0;
+    }
+
+    // Función para verificar si un carácter de apertura y uno de cierre coinciden
+    static bool Coinciden(char apertura, char cierre)
+    {
+        
+        // Devolvemos true si es valido 
+
+    if (apertura == '(' && cierre == ')')
+    {
+        return true;
+    }
+    else if (apertura == '[' && cierre == ']')
+    {
+        return true;
+    }
+    else if (apertura == '{' && cierre == '}')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+
+    }
+}
